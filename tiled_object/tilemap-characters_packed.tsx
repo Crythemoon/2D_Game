09761fe45a5c883cd33bfc06d59d890107822c07
Crythemoon <crythemoon11@gmail.com>
2023-10-14 16:03:53 -0600

    v1

--- tiled_object/tilemap-characters_packed.tsx
+++ tiled_object/tilemap-characters_packed.tsx
@@ -1,4 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="tilemap-characters_packed" tilewidth="72" tileheight="72" tilecount="27" columns="9">
  <image source="../game_object/NPC/tile_map/tilemap-characters_packed.png" width="648" height="216"/>
+ <tile id="21">
+  <animation>
+   <frame tileid="21" duration="300"/>
+   <frame tileid="22" duration="300"/>
+   <frame tileid="23" duration="300"/>
+  </animation>
+ </tile>
 </tileset>
