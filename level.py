80271f46fa3d4dc0a65bd174ae508fca3981e84e
Crythemoon <crythemoon11@gmail.com>
2023-10-25 00:33:01 -0600

    v1

--- level.py
+++ level.py
@@ -16,55 +16,7 @@ LAYER_NAME_ENEMY = "Enemies"
 LAYER_NAME_PLAYER = "Player"
 LAYER_NAME_JUMPABLE_PLATFORM = "Jump Platforms"
 
-class number_level():
-    def __init__(self,level):
-        self.level = level
-        self.background_path = None
-        self.player_sprite_list = []
-
-    def level_background(self,background_path):
-        self.background_path = background_path
-
-    def level_player(self,player_sprite):
-        self.player_sprite = player_sprite
-
-    def level_tile_map(self,map_path,layer_option):
-        self.map_path = map_path
-        self.layer_option = layer_option
-
-def level_1():
-    level = number_level(1)
-
-    player_sprite = object_initialize.Player_Model()
-    player_sprite.center_x = 100
-    player_sprite.center_y = 100
-    player_sprite.scale = 1
-    level.level_player(player_sprite)
-
-    tile_map_path = f'{GAME_DIRECTORY}\\level\\level_1.tmx'
-
-    layer_option = {
-        LAYER_NAME_PLATFORMS:{
-            "use_spatial_hash": True
-        },
-        LAYER_NAME_BACKGROUND:{
-            "use_spatial_hash": False
-        },
-        LAYER_NAME_FOREGROUND:{
-            "use_spatial_hash": False
-        },
-        LAYER_NAME_NPC:{
-            "use_spatial_hash": True
-        }
-    }
-    level.level_tile_map(map_path=tile_map_path,layer_option=layer_option)
-
-    tile_map = arcade.load_tilemap(tile_map_path,1,layer_option)
-    scene = arcade.Scene.from_tilemap(tile_map)
-    def tutorial_character():
-        character_interaction = arcade.check_for_collision(player_sprite,scene["Character"])
-        pass
-    
-
-    return level
-
+class level_1(arcade.View):
+    def __init__(self):
+        super().__init__()
+        
--- test3.py
+++ test3.py
@@ -1,49 +1,35 @@
-import os
-import sys
 import arcade
-import level
+from arcade.application import Window
 
-GAME_DIRECTORY = os.path.abspath(os.path.dirname(__file__))
-sys.path.append(GAME_DIRECTORY)
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
 
-SCREEN_WIDTH = 1680
-SCREEN_HEIGHT = 1080
-
-class GameView(arcade.Window):
-    def __init__(self,width,height):
-        super().__init__(width,height)
-
-        os.chdir(GAME_DIRECTORY)
-
-        self.tile_map = None
-
-        self.scene = None
-
-        self.level_list = []
-
-    def setup(self):
-        room = level.level_1()
-        self.level_list.append(room)
-
-        self.current_room = 0
-
-        self.tile_map = arcade.load_tilemap(
-            self.level_list[self.current_room].map_path,
-            1,
-            self.level_list[self.current_room].layer_option
-        )
-
-        self.scene = arcade.Scene.from_tilemap(self.tile_map)
-
+class MenuView(arcade.View):
+    def on_show_view(self):
+        arcade.set_background_color(None)
+    
     def on_draw(self):
-        self.clear()
+        arcade.draw_text("Hello", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 
+                         arcade.color.BLACK,font_size=50, anchor_x="center")
+        
+    def on_mouse_press(self, _x, _y, _button, _modifiers):
+        game = GameView()
+        self.window.show_view(game)
 
-        self.scene.draw()
+class GameView(arcade.View):
+    def __init__(self):
+        super().__init__()
 
+    def on_show_view(self):
+        arcade.set_background_color(arcade.color.AMAZON)
+    
+    def on_draw(self):
+        arcade.draw_text("Hello", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 
+                         arcade.color.BLACK,font_size=50, anchor_x="center")
+    
 def main():
-    window = GameView(SCREEN_WIDTH,SCREEN_HEIGHT)
-    window.setup()
+    window = arcade.Window(SCREEN_WIDTH,SCREEN_HEIGHT,"Hello")
+    menu = MenuView()
+    window.show_view(menu)
     arcade.run()
-
-if __name__ == "__main__":
-    main()
+main()
